ad42202a9dcfb39e1945cfd8d2c0f76cd6127bb6
maxgfr <25312957+maxgfr@users.noreply.github.com>
2022-01-01 15:56:25 +0100

    feat: add lib

---
+++ src/components/Camera.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import {widthPercentageToDP as wp} from 'react-native-responsive-screen';
+
+type Props = {
+  onRead: (e: any) => void;
+  topComponent?: any;
+  bottomComponent?: any;
+  containerStyle?: any;
+  children?: any;
+};
+
+export const Camera = (props: Props) => {
+  return (
+    <View style={{...styles.containerStyle, ...props.containerStyle}}>
+      <QRCodeScanner
+        onRead={props.onRead}
+        topContent={props.topComponent}
+        bottomContent={props.bottomComponent}
+        cameraStyle={styles.cameraStyle}
+        containerStyle={styles.cameraStyle}
+        showMarker={true}
+        cameraProps={{ratio: '1:1'}}
+        reactivate={true}
+        vibrate={false}
+      />
+      {props.children}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  containerStyle: {
+    height: wp('100%'),
+    width: wp('100%'),
+  },
+  cameraStyle: {
+    alignSelf: 'center',
+    justifyContent: 'center',
+    overflow: 'hidden',
+  },
+});
